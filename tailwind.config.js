eb7f778e56a6cf3243bc5508f8c400f7ef2b1ed3
kaliwa <49640654+kaliwa@users.noreply.github.com>
2020-09-25 19:13:34 +0700

    📦 NEW: basic style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#333',
-      },
-    },
-  },
-  variants: {},
-  plugins: [],
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: ['./components/**/*.{tsx}', './pages/**/*.{tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'accent-1': '#333',
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
 }
