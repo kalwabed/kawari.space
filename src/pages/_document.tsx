51ce0ccb2559e84f645d63602f3a3e2f77780957
kaliwa <49640654+kaliwa@users.noreply.github.com>
2020-09-26 23:00:31 +0700

    📦 NEW: manifest, icons, and configure it

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,8 +14,37 @@ class MyDocument extends Document {
 
 	render() {
 		return (
-			<Html>
-				<Head />
+			<Html lang='en'>
+				<Head>
+					<meta charSet='UTF-8' />
+					<meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+
+					<link
+						rel='shortcut icon'
+						href='/icons/favicon.ico'
+						type='image/x-icon'
+					/>
+					<link rel='manifest' href='/manifest.webmanifest' />
+					<link
+						href='/icons/favicon-32x32.png'
+						rel='icon'
+						sizes='32x32'
+						type='image/png'
+					/>
+					<link
+						href='/icons/favicon-16x16.png'
+						rel='icon'
+						sizes='16x16'
+						type='image/png'
+					/>
+					<link
+						rel='apple-touch-icon'
+						sizes='180x180'
+						href='/apple-touch-icon.png'
+						type='image/png'
+					/>
+				</Head>
+
 				<body className='bg-dark text-light font-primary'>
 					<Main />
 					<NextScript />
