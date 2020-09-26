7198bfb1db64debf347ffdaab0d0f12cff02749d
kaliwa <49640654+kaliwa@users.noreply.github.com>
2020-09-26 21:46:28 +0700

    👌 IMPROVE: navbar done!

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -17,17 +17,27 @@ const links = [
 
 export default function Nav({ page = 'whoami' }) {
 	return (
-		<nav>
-			<ul className='flex justify-center items-center p-4 bg-primary'>
-				{links.map(link => (
-					<li className='p-1 mr-2' key={link.label}>
-						<Link href={link.href}>
-							<a className='no-underline'>{link.label}</a>
+		<header>
+			<div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center bg-dark'>
+				<Link href='/'>
+					<a className='flex title-font font-medium items-center mb-4 md:mb-0'>
+						<span className='ml-3 text-xl'>kawari</span>
+					</a>
+				</Link>
+				<nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-light flex flex-wrap items-center text-base justify-center'>
+					{links.map(link => (
+						<Link href={link.href} key={link.label}>
+							<a href={link.href} className='mr-5'>
+								{link.label}
+								{link.label === page ? <div className='link-active' /> : null}
+							</a>
 						</Link>
-						<div className={`link${link.label === page ? '-active' : null}`} />
-					</li>
-				))}
-			</ul>
-		</nav>
+					))}
+				</nav>
+				<div className='inline-flex items-center text-base mt-4 md:mt-0'>
+					<img src='/img/github-logo.png' alt='GitHub logo' className='w-5' />
+				</div>
+			</div>
+		</header>
 	)
 }
