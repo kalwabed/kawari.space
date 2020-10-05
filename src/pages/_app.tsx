44cfec6c139b190c1137b33799e6fc9a102d8d84
kaliwa <49640654+kaliwa@users.noreply.github.com>
2020-10-05 07:57:03 +0700

    📦 NEW: add NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import '@/styles/index.css'
 import React from 'react'
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
+import NProgress from 'nprogress'
+import Router from 'next/router'
 
 import Footer from '@/components/Footer'
 import SEO from '../../next-seo.config'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function App({ Component, pageProps }: AppProps) {
   return (
