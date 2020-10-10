7fb6b9261ece8b0f317c0da19c59ee5f869c0985
kaliwa <49640654+kaliwa@users.noreply.github.com>
2020-10-10 19:56:48 +0700

    👌 IMPROVE: project page

--- site-config.ts
+++ site-config.ts
@@ -5,19 +5,19 @@ const projects: Project[] = [
   {
     title: 'Tuan Tanah',
     description:
-      'Web aplikasi yang ditujukan sebagai tempat bertemunya antara penjual tanah yang memasarkan propertinya dan calon pembeli.',
-    image: '/assets/img/example.jpg',
+      'A web application that is intended as a meeting place between land sellers who are marketing their properties and potential buyers.',
+    image: '/assets/img/tuantanah.png',
     stack: [express, react, bootstrap],
     demoUrl: 'https://tuantanah.vercel.app',
     source: 'https://github.com/kaliwa/tuantanah-website'
   },
   {
-    title: 'My Portofolio',
-    description: 'Situs pribadi saya yang sedang anda akses sekarang :)',
-    image: '/assets/img/aku.png',
-    stack: [nextjs, react, tailwindcss],
-    demoUrl: 'https://kawari.space',
-    source: 'https://github.com/kaliwa/kawari.space'
+    title: 'Kelas RPL 1',
+    description: 'My class official website which lists students and other features that are still in development.',
+    image: '/assets/img/kitarpl1.png',
+    stack: [express, react, bootstrap],
+    demoUrl: 'https://kelasrpl.vercel.app',
+    source: 'https://github.com/kaliwa/kelasrpl'
   }
 ]
 
